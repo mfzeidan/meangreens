647348ea3fd6304eb919161d744b2fe65f8cbab3
Mark Zeidan <mfzeidan@gmail.com>
2016-12-02 14:42:22 -0500

    Update plant_sensor.ino

--- plant_sensor.ino
+++ plant_sensor.ino
@@ -10,9 +10,56 @@
 #define TRIGGER 5
 #define ECHO 4
 
-const char* ssid = "****";
-const char* password = "****";
-const char* mqtt_server = "67.205.148.211";
+#define wifi_ssid "Verizon-MiFi6620L-E739" //fill this
+#define wifi_password "015207b8" //fill this
+#define mqtt_server "162.243.161.166" //fill this
+#define mqtt_user "" // no fill necessary
+#define mqtt_password "" // no fill necessary
+#define topic1 "test"
+#define mqtt_client_name "EAS1" //this needs to be unique for each unit
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(wifi_ssid);
+    WiFi.begin(wifi_ssid, wifi_password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+void reconnect() {
+    // Loop until we're reconnected
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (client.connect(mqtt_client_name)) { //* See //NOTE below
+            Serial.println("connected");
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
+
+void pubMQTT(String topic,String topic_val){
+    Serial.print("Newest topic " + topic + " value:");
+    Serial.println(String(topic_val).c_str());
+    client.publish(topic.c_str(), String(topic_val).c_str(), true);
+}
+    
+
 
 // temp Data wire is plugged into pin 2 on the Arduino
 
@@ -24,7 +71,23 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
-//define distance variables
+//This is a unused function that just handles receiving mqtt messages
+
+void callback(char* topic, byte* payload, unsigned int length) {
+ Serial.print("Message arrived [");
+ Serial.print(topic);
+ Serial.print("] ");
+ for (int i=0;i<length;i++) {
+  char receivedChar = (char)payload[i];
+  Serial.print(receivedChar);
+  if (receivedChar == '0')
+  // ESP8266 Huzzah outputs are "reversed"
+  digitalWrite(ledPin, HIGH);
+  if (receivedChar == '1')
+   digitalWrite(ledPin, LOW);
+  }
+  Serial.println();
+}
 
 
  
@@ -44,6 +107,9 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+    if (!client.connected()) {
+    reconnect();
+  }
 //set up temp readings
 
   Serial.print(" Requesting temperatures...");
@@ -69,4 +135,6 @@ void loop() {
   Serial.print(distance);
   Serial.println("Centimeter:");
   delay(1000);
+  pubMQTT(topic1, message);
+
 }
