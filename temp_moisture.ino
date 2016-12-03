72e3a7e0a5a4ae1ea0312708fdb38165f189aadb
Mark Zeidan <mfzeidan@gmail.com>
2016-12-03 14:29:25 -0500

    Update temp_moisture.ino

--- temp_moisture.ino
+++ temp_moisture.ino
@@ -1,6 +1,18 @@
 // Include the libraries we need
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#define wifi_ssid "9a8fc8" //fill this
+#define wifi_password "278993750" //fill this
+#define mqtt_server "104.236.210.175" //fill this
+#define mqtt_user "" // no fill necessary
+#define mqtt_password "" // no fill necessary
+#define topic1 "test"
+#define mqtt_client_name "EAS1" //this needs to be unique for each unit
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 5
@@ -12,7 +24,49 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
 
+void setup_wifi() {
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(wifi_ssid);
+    WiFi.begin(wifi_ssid, wifi_password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+void reconnect() {
+    // Loop until we're reconnected
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (client.connect(mqtt_client_name)) { //* See //NOTE below
+            Serial.println("connected");
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
+
+void pubMQTT(String topic,String topic_val){
+    Serial.print("Newest topic " + topic + " value:");
+    Serial.println(String(topic_val).c_str());
+    client.publish(topic.c_str(), String(topic_val).c_str(), true);
+}
+
 void setup() {
+
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
   // put your setup code here, to run once:
   Serial.begin(9600);
     pinMode(A0, INPUT);
@@ -25,6 +79,10 @@ void setup() {
 }
 
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
   // put your main code here, to run repeatedly:
   int humidityRaw = analogRead(A0);
   int humidityReal = map(humidityRaw,1023,0,0,100);
@@ -47,5 +105,7 @@ void loop() {
 
   String message_to_send = String("{") + String("\"DeviceInfo\"") + String(":[{\"DeviceID\":\"") +  String(device_id) + "\", \"Moisture\":\"" + String(humidityReal) + "\", \"temp\":\"" + String(faren) + "\"}]}";
   Serial.println(message_to_send);
-  
+  pubMQTT(topic1, String(message_to_send));
+  delay(10000);
 }
+
