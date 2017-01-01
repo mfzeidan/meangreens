352b7010fe4c23986f491a639bd34e336cff9d87
Mark Zeidan <mfzeidan@gmail.com>
2017-01-01 16:03:38 -0500

    Update greenhouse_code.ino

--- greenhousev2/greenhouse_code.ino
+++ greenhousev2/greenhouse_code.ino
@@ -1,7 +1,6 @@
 
 
 
-
 /*
  * written by mz
  * the goal of this script is to accomplish the following tasks
@@ -35,13 +34,19 @@
 //nodeMCU pins we can use - 5,4,2, 14,12,13,15,3,1,10,9
 
 int light1_on_indicator = 5;
-int light1_off_indicator =4;
+//int light1_off_indicator =4;
+////////////
 int light2_wifi_connected_indicator =2;
 int light2_wifi_not_connected_indicator =14;
+////////////
 int light3_growing_progress_on_light =12;
 int light3_growing_progress_off_light =13;
+/////////
 int light4_mqtt_on_light = 12; // green
 int light4_mqtt_off_light = 131; // red
+///////////////
+
+
 int temp_relay_switch = 15;
 int current_temperature=77;
 int desired_temperature=77;
@@ -105,9 +110,26 @@ void light_on_off(int pin){
 }
 
 
+
+void printTemperature(DeviceAddress deviceAddress)
+{
+  float tempC = sensors.getTempC(deviceAddress);
+  if (tempC == -127.00) {
+    Serial.print("Error getting temperature");
+  } else {
+    Serial.print("C: ");
+    Serial.print(tempC);
+    Serial.print(" F: ");
+    Serial.print(DallasTemperature::toFahrenheit(tempC));
+  }
+}
+
 void callback(char* topic, byte* payload, unsigned int length) {
   // Here is what i have been using to handle subscriptions. I took it as a snippet from elsewhere but i cannot credit author as i dont have reference!
     int i = 0;
+      //blink the green light signifying that a message has been received
+    light_on_off(light4_mqtt_on_light);
+    
   Serial.println("Message arrived:  topic: " + String(topic));
   Serial.println("Length: " + String(length,DEC));
   // create character buffer with ending null terminator (string)
@@ -125,27 +147,32 @@ void callback(char* topic, byte* payload, unsigned int length) {
 ///here is where we will parse the json message that's being received to update all variables once a message is received
   JsonObject& root = jsonBuffer.parseObject(msgString);
 
-  const char* sensor1 = root["DeviceID"];
-  const char* sensor2 = root["newgrow_flag"];
-  const char* sensor3 = root["Moisture"];
-  const char* sensor4 = root["temp"];
-  const char* sensor = root["temp"];
+  const char* temperature = root["temperature"];
+  const char* humidity = root["humidity"];
+  const char* light = root["light"];
+  const char* progress_flag = root["inprogress"];
  // Serial.println(root);
-  Serial.println("test"); 
-  Serial.println(sensor1);
-  Serial.println(sensor2);
-  Serial.println(sensor3);
-  Serial.println(sensor4);
-       
-  if (msgString == "1"){    // if there is a "1" published to any topic (#) on the broker then:
-   message_to_print = 78;
-   Serial.println(message_to_print);  
+  //Serial.println("test");
+  Serial.println(temperature);
+  Serial.println(humidity);
+  Serial.println(light);
+  Serial.println(progress_flag);
 
-    
-    digitalWrite(2, !state);     // set pin to the opposite state 
-    Serial.println("Switching LED"); 
+  //flag 1 signifies that there is currently a grow cycle in progress
+
+  if(progress_flag == "1"){
+    digitalWrite(light3_growing_progress_on_light,HIGH);
+    digitalWrite(light3_growing_progress_off_light,LOW);
   }
+
+  //flag 0 signifies that there is currently no grow cycle in progress
+  if (progress_flag == "0"){
+    digitalWrite(light3_growing_progress_on_light,LOW);
+    digitalWrite(light3_growing_progress_off_light,HIGH);
+  }
+       
 }
+
 
 
 
@@ -171,7 +198,6 @@ void setup() {
   
 
     pinMode(light1_on_indicator, OUTPUT);
-    pinMode(light1_off_indicator, OUTPUT);
     pinMode(light2_wifi_connected_indicator, OUTPUT);
     pinMode(light2_wifi_not_connected_indicator, OUTPUT);
     pinMode(light3_growing_progress_on_light, OUTPUT);
@@ -180,12 +206,25 @@ void setup() {
     pinMode(light4_mqtt_on_light, OUTPUT); // green
     pinMode(light4_mqtt_off_light, OUTPUT); // red
     pinMode(temp_relay_switch, OUTPUT);
-  
+//turning all the off indicators to high and turning all the on indicators to low
+
+    digitalWrite(light1_on_indicator, HIGH);
+    digitalWrite(light2_wifi_not_connected_indicator, HIGH);
+    digitalWrite(light3_growing_progress_off_light,HIGH);
+    digitalWrite(light4_mqtt_off_light,HIGH);
+            /////////
+    digitalWrite(light2_wifi_connected_indicator,LOW);
+    digitalWrite(light3_growing_progress_on_light,LOW);
+    digitalWrite(light4_mqtt_on_light,LOW);
+            /////////
+
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
   StaticJsonBuffer<200> jsonBuffer;
+
+
 
   
   WiFi.begin(ssid, password);
@@ -194,11 +233,10 @@ void setup() {
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-     digitalWrite(light2_wifi_not_connected_indicator, HIGH);
     Serial.print(".");
   }
      digitalWrite(light2_wifi_not_connected_indicator, LOW);
-     digitalWrite(light2_wifi_connected_indicator, HIGH);
+     light_on_off(light2_wifi_connected_indicator);
   Serial.println("");
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
@@ -206,9 +244,10 @@ void setup() {
  
   //  connection to broker script.
   if (client.connect("arduinoClient")) {
+    light_on_off(light4_mqtt_on_light);
     //TODO change these
     //client.publish("outTopic","hello world");
-    client.subscribe(topic);   // i should probably just point this to the varialbe above.
+    client.subscribe(topic);   //i should probably just point this to the varialbe above.
   }
 }
 
@@ -218,7 +257,36 @@ void setup() {
   client.loop();
   Serial.println(message_to_print);
   delay(1000);
+  ////////////
+
+
+  Serial.print("Getting temperatures...\n\r");
+  sensors.requestTemperatures();
+  
+  Serial.print("Inside  temperature is: ");
+  printTemperature(insideThermometer);
+  Serial.print("\n\r");
+  Serial.print("Outside temperature is: ");
+  int outside_tempC = sensors.getTempC(outsideThermometer);
+  int outside_tempF = DallasTemperature::toFahrenheit(outside_tempC);
+  int inside_tempC = sensors.getTempC(insideThermometer);
+  int inside_tempF = DallasTemperature::toFahrenheit(inside_tempC);
+  int temp_avg = (outside_tempF + inside_tempF)/2;
+  //number 2 is indoo
+  //int indoor = tempC;
+  Serial.print("\n\r");
+  //Serial.print("Dog House temperature is: ");
+  //printTemperature(dogHouseThermometer);
+  Serial.print("\n\r\n\r");
   
 }
+
+
+//temperature readings CHECK
+    //average the 2 temperature readings into one DONE
+//relay control for light
+//relay control for temperature control pad
+//code to read soil sensor
+//code for the LEDS CHECK but need to edit input pins for the LEDS
 
 
