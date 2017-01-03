5ff592690759e86166c1ca10e7cae6915f1cf391
Mark Zeidan <mfzeidan@gmail.com>
2017-01-03 11:56:29 -0500

    Update email_lookup_temp.py

--- Broker Handler/email_lookup_temp.py
+++ Broker Handler/email_lookup_temp.py
@@ -9,10 +9,60 @@ con = MySQLdb.connect(host='159.203.165.37', port=3306,
 cur = con.cursor()
 
 
-def send_email(email):
+def send_email(email,current_temps):
 	print "im going to now send an email to " + str(email)
+	print current_temps
+	
+	
+	
+def temperature_trigger(devID,device_temp):
 
-def table_lookup(devID):
+	temp_lookup = 'select desired_temp from device_mgmt where deviceID = %s' % (devID)
+
+	try:
+		cur.execute(temp_lookup)
+		rows_temp = cur.fetchall()
+		#print rows
+		sql_list = []
+		for r in rows_temp:
+			print r[0]
+			desired_temps =int(r[0])
+			print device_temp
+	except:
+		con.rollback()
+		print "nopeeeeeeeeeeee"
+
+	if desired_temps < device_temp:
+		print "desired temp"
+		print desired_temps
+		print "device_temp"
+		print device_temp
+		table_lookup(devID,device_temp)
+	else:
+		print "not high enough"
+
+def table_lookup(devID,current_temp):
+
+
+	sqldata = 'select username from device_mgmt where deviceID = %s' % (devID)
+
+	try:
+		cur.execute(sqldata)
+		rows = cur.fetchall()
+		#print rows
+		sql_list = []
+		for r in rows:
+			username_lookup = r[0]
+			sqldata2 = """select email from user where username = '%s'""" % (username_lookup)
+			#print sqldata2
+
+
+	except:
+		con.rollback()
+		print "nopeeeeee"
+
+
+	#find the username to get the email
 
 	sqldata = 'select username from device_mgmt where deviceID = %s' % (devID)
 
@@ -29,12 +79,9 @@ def table_lookup(devID):
 			sqldata2 = """select email from user where username = '%s'""" % (username33)
 			#print sqldata2
 
-
 	except:
 		con.rollback()
 		print "nope"
-
-	print "next step"
 
 	try:
 		cur.execute(sqldata2)
@@ -44,9 +91,9 @@ def table_lookup(devID):
 	except:
 		con.rollback()
 		print "failed"
-
-
 	con.close()
-	send_email(emailToSend)	
+
+	send_email(emailToSend,current_temp)	
 	
-table_lookup(445)
+
+temperature_trigger(445,125)
